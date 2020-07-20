2ea7fc634cddf422e210e1ac1c8ad8ac615a61dd
MikZuit Github <mikexloo@hotmail.com>
2020-07-20 14:27:39 +0200

    remove junk aws install

--- installers/install-aws2-cli.sh
+++ installers/install-aws2-cli.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ -z "$(which aws)" ]]; then
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install --update
-    sudo rm -r ./aws
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+    unzip /tmp/awscliv2.zip
+    sudo /tmp/aws/install --update
+    sudo rm /tmp/awscliv2.zip
+    sudo rm -r /tmp/aws
 fi
