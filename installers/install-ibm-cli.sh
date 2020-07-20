fd5eca5415d78470087a2e31dc627d50af1c403d
MikZuit Github <mikexloo@hotmail.com>
2020-07-20 15:04:23 +0200

    fix EOF ibmcloud

--- installers/install-ibm-cli.sh
+++ installers/install-ibm-cli.sh
@@ -2,5 +2,5 @@
 if [[ -z "$(which ibmcloud)" ]]; then
     curl -sL https://ibm.biz/idt-installer | bash || exit 1
 
-    echo "\n------------> ibm cloud installed succesfully\"
+    echo "\n------------> ibm cloud installed succesfully\n"
 fi
