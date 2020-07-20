677935781c5838ce9b059af1325b6d44cb278bc3
MikZuit Github <mikexloo@hotmail.com>
2020-07-20 18:14:09 +0200

    fix helm yes

--- installers/install-helm.sh
+++ installers/install-helm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ -z "$(which helm)" ]]; then
     curl https://helm.baltorepo.com/organization/signing.asc | sudo apt-key add -
-    sudo apt-get -yqq install apt-transport-https
+    sudo apt-get install apt-transport-https --yes
     echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
     sudo apt-get update
     sudo apt-get install -yqq helm
