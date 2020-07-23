21575c621cf23a8bf9202c4ab4a38703ffd71804
MikZuit Github <mikexloo@hotmail.com>
2020-07-23 12:08:26 +0200

    merge if line from repo and solve conflict

--- installers/install-vagrant.sh
+++ installers/install-vagrant.sh
@@ -2,11 +2,7 @@
 
 if [ -z "$(which vagrant)" ]; then
 
-<<<<<<< HEAD
     curl "https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_linux_amd64.zip" -o "vagrant.zip"
-=======
-    curl "https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_linux_amd64.zip" -o "/tmp/vagrant.zip"
->>>>>>> ae78f79180f48da85832f1a7403e8f28cb5ea25f
 
     unzip -o /tmp/vagrant.zip -d /tmp
 
